59ed65fa46a196d35a240ed3918a3c6f40a843a5
moham <mohammefd.Zeddaouii@gmail.com>
2023-02-27 12:03:44 +0100

    Initial commit

---
+++ demo_flake8.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+print("hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world")
+package_dir = Path(__file__).parent.absolute()
+print(package_dir)
