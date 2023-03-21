33918d218ec60a4d03cdebb33f228f32f561cc9a
moham <mohammefd.Zeddaouii@gmail.com>
2023-03-21 13:30:44 +0100

    extraction

---
+++ train/extraction/tests/extraction_test.py
@@ -0,0 +1,12 @@
+# run from ./train/extraction directory
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        value = 'foo'.upper()
+        expected = 'FOO'
+        self.assertEqual(value, expected)
+
+if __name__ == '__main__':
+    unittest.main()
